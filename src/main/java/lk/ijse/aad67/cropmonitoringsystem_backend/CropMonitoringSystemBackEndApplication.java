671d45b3054def0d6c1d226fdb3fa0fb16cbde69
DILSHAN <dilshanfonseka76@gmail.com>
2024-11-12 00:20:49 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/aad67/cropmonitoringsystem_backend/CropMonitoringSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad67.cropmonitoringsystem_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringSystemBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CropMonitoringSystemBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/aad67/cropmonitoringsystem_backend/CropMonitoringSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad67.cropmonitoringsystem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringSystemBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
