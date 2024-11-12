876ddab33c0ed5cc965d0f16eea63cf6709ab0fc
DILSHAN <dilshanfonseka76@gmail.com>
2024-11-12 13:16:33 +0530

    added field entity

---
+++ src/main/java/lk/ijse/aad67/cropmonitoringsystem_backend/entity/Role.java
@@ -0,0 +1,5 @@
+package lk.ijse.aad67.cropmonitoringsystem_backend.entity;
+
+public enum Role {
+    MANAGER,ADMINISTRATIVE,SCIENTIST
+}
---
+++ src/main/java/lk/ijse/aad67/cropmonitoringsystem_backend/entity/impl/FieldEntity.java
@@ -0,0 +1,32 @@
+package lk.ijse.aad67.cropmonitoringsystem_backend.entity.impl;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.aad67.cropmonitoringsystem_backend.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.awt.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "field")
+public class FieldEntity implements SuperEntity {
+    @Id
+    private String f_Code;
+    private String f_Name;
+    private Point f_Location;
+    private Double f_Size;
+    private List c_Code;
+    private List s_Code;
+    @Column(columnDefinition = "LONGTEXT")
+    private String field_Image1;
+    @Column(columnDefinition = "LONGTEXT")
+    private String field_Image2;
+
+}
