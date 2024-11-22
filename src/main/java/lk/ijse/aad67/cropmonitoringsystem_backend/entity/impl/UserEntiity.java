2792a6732a1053e4f02e096480313c34ba700d85
DILSHAN <dilshanfonseka76@gmail.com>
2024-11-22 21:45:37 +0530

    user entity added

---
+++ src/main/java/lk/ijse/aad67/cropmonitoringsystem_backend/entity/impl/UserEntiity.java
@@ -0,0 +1,6 @@
+package lk.ijse.aad67.cropmonitoringsystem_backend.entity.impl;
+
+import lk.ijse.aad67.cropmonitoringsystem_backend.entity.SuperEntity;
+
+public class UserEntiity implements SuperEntity {
+}
